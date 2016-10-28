9aa3f7d1b484f2963dff0423ca0e0ee8cd4fd858
Elibay <elibay@MacBook-Pro-Elibay.local>
2016-10-28 19:14:47 +0600

    week_1 1 & 2 solved

---
+++ week_1/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+int a[150];
+
+void gen(int i) {
+	if (i < n) {
+		for (int q = 0; q <= k; ++q) {
+			a[i] = q;
+			gen(i + 1);
+		}
+	} else {
+		for (int i = 0; i < n; ++i)
+			cout << a[i];
+		cout << endl;
+	}
+}
+
+int main() {
+	cin >> n >> k;
+
+	gen(0);
+
+	return 0;
+}
